cb1a11f35af5aef5720f6f58a8c2c51a8394af55
4bic <robyne.kiplangat@gmail.com>
2018-07-09 17:38:46 +0300

    restructure the flow

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
-# from flask import Flask, render_template
-#
-# app=Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return "Hey there!"
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+# from flask.ext.heroku import Heroku
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://twitterr:twitterr@localhost/REngine_dev"
+
+# heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+# Create our database model
+class User(db.Model):
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
 
 
-from app import app
+# Set "homepage" to index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Save e-mail to database and send to success page
+@app.route('/prereg', methods=['POST'])
+def prereg():
+    email = None
+    if request.method == 'POST':
+        email = request.form['email']
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(User).filter(User.email == email).count():
+            reg = User(email)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('success.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run()

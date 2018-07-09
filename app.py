a072e2b343238eabf333da83c2d4d8ffc2e1a4b0
4bic <robyne.kiplangat@gmail.com>
2018-07-09 19:15:09 +0300

    we can post users to db

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-from rEngine import config , models
+from rEngine import config #,models
 
 # from flask.ext.heroku import Heroku
 
@@ -19,27 +19,45 @@ class User(db.Model):
     name = db.Column(db.String())
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, email):
+    def __init__(self, name,email):
+        self.name = name
         self.email = email
 
     def __repr__(self):
         return '<E-mail %r>' % self.email
 
+class Results(db.Model):
+    __tablename__ = "keywords"
+    id = db.Column(db.Integer(),primary_key=True)
+    user_name = db.Column(db.String())
+    text = db.Column(db.String())
+    user_created = db.Column(db.String())
+
+    def __init__(self, name,text,created):
+        self.name = user_name
+        self.text = text
+        self.created = user_created
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
 
 # Set "homepage" to index.html
 @app.route('/')
+
 def index():
     return render_template('index.html')
 
-# Save e-mail to database and send to success page
+# Save name & e-mail to database and send to success page
 @app.route('/prereg', methods=['POST'])
 def prereg():
+    name = None
     email = None
     if request.method == 'POST':
+        name = request.form['name']
         email = request.form['email']
         # Check that email does not already exist (not a great query, but works)
         if not db.session.query(User).filter(User.email == email).count():
-            reg = User(email)
+            reg = User(name,email)
             db.session.add(reg)
             db.session.commit()
             return render_template('success.html')
